8d527659a3c7a61a4df415dc0a59f5aec62fc6cd
GustavoRosattoSilva <50531178+GustavoRosattoSilva@users.noreply.github.com>
2021-06-30 15:10:31 -0300

    Create conversor.c

---
+++ Simulação de caça em 2-D/codigos auxiliares/conversor.c
@@ -0,0 +1,131 @@
+char str_1(int numero){
+
+    if (numero == 1){
+        
+        return '1';
+    
+    }
+
+    if (numero == 2){
+        
+        return '2';
+    
+    }
+
+    if (numero == 3){
+        
+        return '3';
+    
+    }
+
+    if (numero == 4){
+        
+        return '4';
+    
+    }
+
+    if (numero == 5){
+        
+        return '5';
+    
+    }
+
+    if (numero == 6){
+        
+        return '6';
+    
+    }
+
+    if (numero == 7){
+        
+        return '7';
+    
+    }
+
+    if (numero == 8){
+        
+        return '8';
+    
+    }
+
+    if (numero == 9){
+        
+        return '9';
+    
+    }
+
+    if (numero == 10){
+        
+        return '0';
+    
+    }
+
+    return ' ';
+
+}
+
+int int_1(char carctere){
+
+    if (carctere == '1'){
+        
+        return 1;
+    
+    }
+
+    if (carctere == '2'){
+        
+        return 2;
+    
+    }
+
+    if (carctere == '3'){
+        
+        return 3;
+    
+    }
+
+    if (carctere == '4'){
+        
+        return 4;
+    
+    }
+
+    if (carctere == '5'){
+        
+        return '5';
+    
+    }
+
+    if (carctere == '6'){
+        
+        return 6;
+    
+    }
+
+    if (carctere == '7'){
+        
+        return 7;
+    
+    }
+
+    if (carctere == '8'){
+        
+        return 8;
+    
+    }
+
+    if (carctere == '9'){
+        
+        return 9;
+    
+    }
+
+    if (carctere == '0'){
+        
+        return 10;
+    
+    }
+
+    return 0;
+
+}
